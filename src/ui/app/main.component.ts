215f403c65621ba39cd28f1a6fa782d035e79eec
Sam Kumar <skumarpadmanaban@blackbook.com>
2016-12-01 15:03:44 -0500

    Initial UI shell

---
+++ src/ui/app/main.component.ts
@@ -0,0 +1,9 @@
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'main-container',
+  template: require('./main.view.pug')
+})
+export class MainComponent {
+
+}
